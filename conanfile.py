03d6b53a6917e1a8151a7a22a130359b0fe16be4
Jerry W <jerrywiltse@gmail.com>
2017-08-20 00:43:55 -0400

    enabled short paths

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,7 @@ from conans import ConanFile, tools, os
 class BoostStatic_AssertConan(ConanFile):
     name = "Boost.Static_Assert"
     version = "1.64.0"
+    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-static_assert"
     source_url = "https://github.com/boostorg/static_assert"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
@@ -12,7 +13,7 @@ class BoostStatic_AssertConan(ConanFile):
     
     def source(self):
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
                      .format(self.version, lib_short_name)) 
 
     def package(self):
