50142655640f60e8c62d8a65b4dfac63c898f3f6
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:13 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,11 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostStatic_AssertConan(base.BoostBaseConan):
     name = "boost_static_assert"
     url = "https://github.com/bincrafters/conan-boost_static_assert"
     lib_short_names = ["static_assert"]
     header_only_libs = ["static_assert"]
-    b2_requires = [
-        "boost_config",
-    ]
-
-
+    b2_requires = ["boost_config"]
