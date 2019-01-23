0a40a4674eabc3782b03aac8cb0e806047bef71d
Jerry W <jerrywiltse@gmail.com>
2019-01-22 21:56:58 -0500

    use clang 5.0 for header only, add clang 7 to others, implement versions in recipes, conan no longer allows inheriting version, refresh travis badge, use .ci dir, python 3.6 in appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.67.0@bincrafters/testing")
 
 class BoostStatic_AssertConan(base.BoostBaseConan):
     name = "boost_static_assert"
+    version = "1.67.0"
     url = "https://github.com/bincrafters/conan-boost_static_assert"
     lib_short_names = ["static_assert"]
     header_only_libs = ["static_assert"]
